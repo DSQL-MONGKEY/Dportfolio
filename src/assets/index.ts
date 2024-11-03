c46744a411a718f94858bb7651bf5e3d480018b6
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-03 21:16:06 +0700

    import assets

---
+++ src/assets/index.ts
@@ -0,0 +1,65 @@
+
+import dlogo from './dlogo.png';
+import dimas from './dimas-photo.png';
+// import myCv from './dimas-prasetyo-CV.pdf'
+import agrokimia from './agrokimia.webp'
+import lagundi from './lagundi.png'
+import bangkit from './bangkit.png'
+import lepkom from './lepkom.png'
+
+// Certificates
+import androidBasic from './certificates/Android Basic.webp'
+import backendBeginner from './certificates/Backend Application Beginner.webp'
+import frontendBeginner from './certificates/Frontend Beginner.webp'
+import frontendReact from './certificates/Frontend React.webp'
+import jsBasic from './certificates/JS Basic.webp'
+import kotlinBasic from './certificates/Kotlin Basic.webp'
+import projectManagement from './certificates/Project Management Basic.webp'
+import reactApp from './certificates/React App.webp'
+// import bnsp from './certificates/BNSP Web Programming.jfif'
+import sql from './certificates/SQL.webp'
+import sofw from './certificates/Software Engineer Basic.webp'
+
+
+// Projects
+import uiux from './projects/uiuxfo.png'
+import fo from './projects/fo.png'
+import valo from './projects/valoranism.png'
+import notion from './projects/notion.webp'
+import ytclone from './projects/ytclone.webp'
+import google from './projects/google.webp'
+import porto from './projects/porto.png'
+import cekit from './projects/cekit.png'
+import puskesmas from './projects/puskesmas.png'
+import xclone from './projects/xclone.webp'
+
+export {
+   // myCv,
+   dimas,
+   dlogo,
+   agrokimia,
+   lagundi,
+   bangkit,
+   lepkom,
+   androidBasic,
+   backendBeginner,
+   frontendBeginner,
+   jsBasic,
+   kotlinBasic,
+   projectManagement,
+   reactApp,
+   // bnsp,
+   frontendReact,
+   sql,
+   sofw,
+   uiux,
+   fo,
+   valo,
+   ytclone,
+   notion,
+   google,
+   porto,
+   cekit,
+   puskesmas,
+   xclone
+};
