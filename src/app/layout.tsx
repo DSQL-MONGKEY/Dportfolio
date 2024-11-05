cceafae649b74f49ebbf8b28a221e8ac3ecad91d
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-05 17:52:38 +0700

    feat<>: wrap children with layouts component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,7 @@
-
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+import Layouts from "@/components/layouts";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Dportfolio",
@@ -26,17 +14,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`flex h-screen flex-col md:flex-row md:overflow-hidden`}
-      >
-        <div className="w-full flex-none md:w-64">
-          <Navbar darkTheme={false}   />
-        </div>
-
-        <div className="grow">
-          {children}
-        </div>
+    <html lang="en" suppressHydrationWarning={true}>
+      <body>
+        <main
+          className={`flex h-screen flex-col md:flex-row md:overflow-hidden`}
+        >
+          <Layouts>
+            {children}
+          </Layouts>
+        </main>
       </body>
     </html>
   );
