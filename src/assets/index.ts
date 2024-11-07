d8602c9044eabb8043a178cf13ae6f192aa76681
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-07 21:34:46 +0700

    blank space

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,7 @@
 
 import dlogo from './dlogo.png';
 import dimas from './dimas-photo.png';
+
 // import myCv from './dimas-prasetyo-CV.pdf'
 import agrokimia from './agrokimia.webp'
 import lagundi from './lagundi.png'
@@ -16,6 +17,7 @@ import jsBasic from './certificates/JS Basic.webp'
 import kotlinBasic from './certificates/Kotlin Basic.webp'
 import projectManagement from './certificates/Project Management Basic.webp'
 import reactApp from './certificates/React App.webp'
+
 // import bnsp from './certificates/BNSP Web Programming.jfif'
 import sql from './certificates/SQL.webp'
 import sofw from './certificates/Software Engineer Basic.webp'
