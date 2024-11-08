769635af821afba34ef6bca0f4e497e6450b0f9d
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-08 22:56:37 +0700

    feat<>: add extend theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -55,7 +55,50 @@ const config: Config = {
 				lg: 'var(--radius)',
 				md: 'calc(var(--radius) - 2px)',
 				sm: 'calc(var(--radius) - 4px)'
-			}
+			},
+			keyframes: {
+				'accordion-down': {
+				from: { height: '0' },
+				to: { height: 'var(--radix-accordion-content-height)' }
+				},
+				'accordion-up': {
+				from: { height: 'var(--radix-accordion-content-height)' },
+				to: { height: '0' }
+				},
+				slide: {
+				'0%': { transform: 'translateX(0)' },
+				'20%': { transform: 'translateX(-100%)' },
+				'50%': { transform: 'translateX(-100%)' },
+				'90%': { transform: 'translateX(5%)' },
+				'100%': { transform: 'translateX(0)' }
+				},
+				slideInfinite: {
+				'0%': { transform: 'translateX(0)' },
+				'100%': { transform: 'translateX(-300%)' }
+				},
+				rainArrow: {
+				'0%': { transform: 'translateY(-10%)' },
+				'50%': { transform: 'translateY(10%)' },
+				'100%': { transform: 'translateY(-10%)' }
+				},
+				enterLeft: {
+				'0%': { transform: 'translateX(100px)', opacity: '0' },
+				'100%': { transform: 'translateX(0)', opacity: '1' }
+				},
+				rightInfinite: {
+				'0%': { transform: 'translateX(-150px)' },
+				'100%': { transform: 'translateX(150px)' }
+				}
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out',
+				'slide-card': 'slide 3s 3s ease-in-out',
+				'slide-infinite': 'slideInfinite 100s linear infinite',
+				'rain-arrow': 'rainArrow 1s ease-out infinite',
+				'enter-left': 'enterLeft 0.5s ease-in-out',
+				'right-infinite': 'rightInfinite 2s linear infinite'
+			},
 		},
 		fontFamily: {
 			outfit: ['var(--font-outfit)'],
