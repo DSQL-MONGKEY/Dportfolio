aaf96a5f117f88aaaaac3baaa5a7b96d6d866aeb
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-09 19:01:21 +0700

    feat<>: create toggle themes component

---
+++ src/components/elements/ThemeToggleIcon.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import React from 'react'
+import { useTheme } from 'next-themes'
+import { motion } from 'framer-motion';
+import { IoIosCloudyNight } from "react-icons/io";
+import { IoPartlySunnySharp } from "react-icons/io5";
+
+const ThemeToggleIcon = () => {
+   const { setTheme, resolvedTheme } = useTheme();
+
+   const toggleTheme = () => {
+      setTheme(resolvedTheme === 'light' ? 'dark' : 'light')
+   }
+
+   return (
+      <motion.button
+         initial={{ opacity: 0, scale: 0.5 }}
+         animate={{ opacity: 1, scale: 1 }}
+         transition={{ duration: 0.5, delay: 0.5 }}
+         onClick={toggleTheme}
+         className={'rounded-xl bg-white p-2 dark:bg-neutral-800'}
+      >
+         {resolvedTheme === 'dark' ? <IoIosCloudyNight /> : <IoPartlySunnySharp />}
+      </motion.button>
+   )
+}
+
+export default ThemeToggleIcon
