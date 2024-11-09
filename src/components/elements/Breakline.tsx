4704166ecaf6487d498c2a5ee7932d972ce4db71
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-09 19:00:13 +0700

    feat<>: create breakline component

---
+++ src/components/elements/Breakline.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+interface BreaklineProps {
+   className: string,
+   [propName: string]: string | undefined
+}
+
+const Breakline = ({ className = '', ...others }: BreaklineProps) => {
+   return (
+      <div
+         className={`border-t border-gray-700 dark:border-gray-300 my-4 ${className}`}
+         {...others}
+      ></div>
+   )
+}
+
+export default Breakline
