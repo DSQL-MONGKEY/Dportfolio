0b48ad6a6107748a404b54e5d6d009cf328442c9
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-09 19:04:11 +0700

    Initial create

--- src/components/layouts/sidebar/Profile.tsx
+++ src/components/layouts/sidebar/Profile.tsx
@@ -1,9 +1,13 @@
+import ThemeToggleIcon from '@/components/elements/ThemeToggleIcon'
 import React from 'react'
 
 const Profile = () => {
+
+   
+
    return (
       <div>
-         
+         <ThemeToggleIcon/>
       </div>
    )
 }
