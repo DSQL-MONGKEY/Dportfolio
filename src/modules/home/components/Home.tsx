095e20006a5ddff6504ba8db30e678860a64cc3a
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-09 19:06:29 +0700

    initial create

---
+++ src/modules/home/components/Home.tsx
@@ -0,0 +1,14 @@
+import TrailBorder from '@/components/elements/TrailBorder'
+import React from 'react'
+
+const Home = () => {
+   return (
+      <div className="">
+         <TrailBorder className='bg-white' duration='5s' color='#000000' trailSize='lg'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam eum provident assumenda accusamus culpa optio, quidem explicabo dolore dicta consequuntur quasi necessitatibus blanditiis facere sint illo corporis deleniti repellat fugit.
+         </TrailBorder>
+      </div>
+   )
+}
+
+export default Home
