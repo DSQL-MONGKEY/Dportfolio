9c843629969d7c78b0a42b7df5c1bebb000e1017
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-10 21:02:52 +0700

    feat<>: add content

--- src/modules/home/components/Intro.tsx
+++ src/modules/home/components/Intro.tsx
@@ -18,17 +18,17 @@ const Intro = () => {
 
             <div className="space-y-4">
                <ul className='flex gap-3 ml-5 my-3 dark:text-white '>
-                  <li className='flex gap-1 items-center'>
+                  <li className='flex gap-1 items-center font-poppins'>
                      <div className='w-3 h-3 dark:bg-calmBlue bg-mainDark rounded shadow-xl'></div>
                      Frontend/Fullstack
                   </li>
-                  <li className='flex gap-1 items-center'>
-                     <div className='w-3 h-3 dark:bg-calmBlue bg-mainDark rounded'></div>
+                  <li className='flex gap-1 items-center font-poppins'>
+                     <div className='w-3 h-3 dark:bg-calmBlue bg-mainDark rounded '></div>
                      Depok, Indonesia
                   </li>
                </ul>
-               <p className="leading-loose dark:text-white">
-               Software Engineer driven by a passion for building modern user-focused applications that blend intuitive design with robust functionality. With expertise in JavaScript/TypeScript, I combine creativity with technical precision to deliver scalable, efficient, and highly readable applications. Solid understanding of other web technologies and an enthusiasm for solving complex challenges, always striving to find innovative and effective solutions for every project.
+               <p className="leading-loose text-neutral-900 font-outfit dark:text-white">
+                  Software Engineer driven by a passion for building modern user-focused applications that blend intuitive design with robust functionality. With expertise in JavaScript/TypeScript, I combine creativity with technical precision to deliver scalable, efficient, and highly maintainable applications. 
                </p>
             </div>
       </section>
