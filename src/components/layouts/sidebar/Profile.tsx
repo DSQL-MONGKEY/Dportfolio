dd131c794c0d9a68e2b75ac39d28e58b35f4048d
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-12 16:19:20 +0700

    feat<>: add image to be render

--- src/components/layouts/sidebar/Profile.tsx
+++ src/components/layouts/sidebar/Profile.tsx
@@ -1,15 +1,46 @@
+"use client"
+
+import React, { useEffect } from 'react'
+import ProfileHeader from './ProfileHeader'
+import useIsMobile from '@/hooks/useIsMobile'
+import { useMenu } from '@/stores/menu'
+import clsx from 'clsx'
 import ThemeToggleIcon from '@/components/elements/ThemeToggleIcon'
-import React from 'react'
 
 const Profile = () => {
-
+   const isMobile = useIsMobile();
+   const imageSize = isMobile ? 40 : 100;
+   const { isOpen, toggleMenu } = useMenu(); 
    
+   useEffect(() => {
+      if(isOpen) {
+         document.body.style.overflow = 'hidden'
+      } else {
+         document.body.style.overflow = 'auto'
+      }
+
+      return () => {
+         document.body.style.overflow = 'auto'
+      }
+
+   },[isOpen])
 
    return (
-      <div>
-         <ThemeToggleIcon />
-         <div className="flex">
-            
+      <div className={clsx(
+         'fixed z-20 w-full bg-white p-5 shadow-sm dark:border-b dark:border-neutral-800 dark:bg-dark lg:relative lg:border-none lg:!bg-transparent lg:p-0 xl:shadow-none',
+         isOpen && 'pb-0'
+      )}>
+         <div className="flex flex-col justify-center">
+            <ProfileHeader expandMenu={isOpen} imageSize={imageSize} />
+            <div className='text-center'>
+               <h2>Dimas Prasetyo</h2>
+               <p>Software Engineer</p>
+            </div>
+            {isMobile && (
+               <div>
+                  <ThemeToggleIcon />
+               </div>
+            )}
          </div>
       </div>
    )
