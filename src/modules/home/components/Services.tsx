365ea945c63cec921893a6b6fe079371331801cb
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-13 21:33:01 +0700

    feat<>: add card component

--- src/modules/home/components/Services.tsx
+++ src/modules/home/components/Services.tsx
@@ -1,5 +1,7 @@
+import { services } from '@/common/constants/constants';
 import SectionHeading from '@/components/elements/SectionHeading'
 import SectionSubHeading from '@/components/elements/SectionSubHeading';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/Card';
 import React from 'react'
 
 import { SiCloudflareworkers } from "react-icons/si";
@@ -12,6 +14,19 @@ const Services = () => {
             <p>The services that i can serve</p>
          </SectionSubHeading>
          
+         <div className="grid grid-cols-2 gap-3 mt-6">
+            {services.map((item, idx) => (
+               <Card key={idx}>
+                  <CardContent>
+                     
+                  </CardContent>
+                  <CardHeader>
+                     <CardTitle>{item.title}</CardTitle>
+                     <CardDescription>{item.subTitle}</CardDescription>
+                  </CardHeader>
+               </Card>
+            ))}
+         </div>
       </section>
    )
 }
