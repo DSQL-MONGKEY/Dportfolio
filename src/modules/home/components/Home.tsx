025105912dd79a1df10ac4746935bb70555c6b2f
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-13 21:32:41 +0700

    feat<>: remove card render testing

--- src/modules/home/components/Home.tsx
+++ src/modules/home/components/Home.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import Intro from './Intro'
 import Breakline from '@/components/elements/Breakline'
-import NeoButton from '@/components/ui/NeoButton'
 import Services from './Services'
-import { Card, CardContent } from '@/components/ui/Card'
 
 const Home = () => {
    return (
@@ -12,12 +10,6 @@ const Home = () => {
          <Breakline />
          <Services />
          <Breakline />
-        
-         <Card>
-            <CardContent>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium incidunt odio corrupti facilis, molestias sed, nisi dolore perferendis sequi, est veritatis accusantium expedita repellat. Minus neque repudiandae id ut molestias.
-            </CardContent>
-         </Card>
       </>
    )
 }
