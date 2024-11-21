e0726cc568f437a0da1d12d25f93e89d5ac4f9e7
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-21 23:01:12 +0700

    feat<>: create articlesCard component

---
+++ src/modules/home/components/ArticleCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ArticleCard = () => {
+   return (
+      <button className='relative transition hover:scale-95 hover:duration-300'>
+         <div className="relative z-10 flex h-max min-w-[250px] flex-col items-start space-y-1">
+            
+         </div>
+      </button>
+   )
+}
+
+export default ArticleCard
