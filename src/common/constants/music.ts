f6f598f4e467fe395e9340861b88d62c29f4e51b
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-21 22:59:23 +0700

    feat<>: add and create musicPlaylist for musicBox component

---
+++ src/common/constants/music.ts
@@ -0,0 +1,10 @@
+
+
+export const musicPlaylist = [
+   {
+      musicCover: '',
+      music:   '/music/Die with a Smile.mp3',
+      artist: 'Bruno Mars ft Lady Gaga',
+      title: 'Die with a Smile'
+   }
+]
