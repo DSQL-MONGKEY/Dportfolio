2c5ea1515b69875f42106af20a1a0c94e2fcbe44
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-21 23:04:51 +0700

    style<>: change bg color

--- src/components/elements/IllustrationContainer.tsx
+++ src/components/elements/IllustrationContainer.tsx
@@ -4,7 +4,7 @@ import React, { ReactNode } from 'react'
 const IllustrationContainer = ({ children }: { children: ReactNode }) => {
    return (
       <div className="h-36 w-52 overflow-hidden rounded-lg border dark:border-neutral-700">
-         <div className="flex justify-center items-center gap-2  p-3 pb-0 dark:bg-neutral-950">
+         <div className="flex justify-center items-center gap-2  p-3 pb-0 dark:bg-neutral-950 h-full">
             {children}
          </div>
       </div>
