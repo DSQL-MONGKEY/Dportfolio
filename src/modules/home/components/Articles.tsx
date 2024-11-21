0f003f7f65146343e98edce9536229c5fe8c7447
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-21 23:02:57 +0700

    feat<>: wrap the heading with div and spacing vertically

--- src/modules/home/components/Articles.tsx
+++ src/modules/home/components/Articles.tsx
@@ -6,10 +6,16 @@ import { RxCrumpledPaper } from "react-icons/rx";
 const Articles = () => {
    return (
       <section>
-         <SectionHeading icon={<RxCrumpledPaper />} title={"Articles"} />
-         <SectionSubHeading>
-            <p>You can read my articles and I hope that&apos;s all usefull</p>
-         </SectionSubHeading>
+         <div className="space-y-2">
+            <SectionHeading icon={<RxCrumpledPaper />} title={"Articles"} />
+            <SectionSubHeading>
+               <p>You can read my articles and I hope that&apos;s all usefull</p>
+            </SectionSubHeading>
+         </div>
+
+         <div className='no-scrollbar mt-4 flex h-40 flex-row space-x-3 overflow-y-hidden overflow-x-scroll pt-2'>
+            
+         </div>
       </section>
    )
 }
