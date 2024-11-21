5ed7f64b78640a59e58fb5d4a43abd015fdc3cf3
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-21 22:58:31 +0700

    feat<>: add icon for services

--- src/common/constants/constants.tsx
+++ src/common/constants/constants.tsx
@@ -1,12 +1,15 @@
 import { GoHome, GoQuestion, GoProject, GoWorkflow, GoCodeReview, GoBug, GoClockFill, GoDiscussionClosed, GoStarFill, GoNorthStar } from "react-icons/go"
 import { BsChatRightText, BsInstagram, BsLinkedin, BsGithub } from "react-icons/bs"
-import { SiMui, SiHtml5, SiXampp, SiIntellijidea, SiAndroidstudio, SiCisco} from "react-icons/si"
+import { SiMui, SiHtml5, SiXampp, SiIntellijidea, SiAndroidstudio, SiCisco, SiTestinglibrary} from "react-icons/si"
 import { IoSchoolOutline, IoLogoJavascript } from "react-icons/io5"
 import { FaCss3, FaPhp, FaReact, FaBootstrap, FaGitAlt, FaFigma, FaNodeJs, FaPython } from "react-icons/fa"
 import { TbBrandTypescript, TbBrandTailwind, TbBrandNextjs, TbBrandMysql, TbBrandMongodb, TbBrandVite, TbBrandNotion, TbBrandVscode, TbBrandKotlin, TbBrandGmail, TbBrandGolang} from "react-icons/tb"
 import { GrDocker } from "react-icons/gr";
 import { androidBasic, backendBeginner, bangkit,  cekit, fo, frontendBeginner, frontendReact, google, jsBasic, kotlinBasic, lepkom, notion, porto, projectManagement, puskesmas, reactApp, sofw, sql, uiux, valo, xclone, ytclone } from "@assets/index"
-import { PiCoffeeDuotone } from "react-icons/pi";
+import { PiCoffeeDuotone, PiNetworkFill  } from "react-icons/pi";
+
+import { CiMobile3 } from "react-icons/ci";
+import { MdWeb } from "react-icons/md";
 
 import { MenuItemProps } from "../types/menu"
 
@@ -55,24 +58,28 @@ export const navLinks: MenuItemProps[] = [
 
 export const services = [
    {
+      icon: <MdWeb /> ,
       id: '72d7892ede',
       title: 'Website Development',
       description: 'Create stunning website, user-friendly design with modern technologies '
    },
    {
+      icon: <CiMobile3 />,
       id: 'cd5fc36877',
       title: 'Mobile Development',
       description: 'Create cross-platform and reliable application using React-Native'
    },
    {
+      icon: <PiNetworkFill />,
       id: 'a9292e0942',
       title: 'IoT (Internet of Things)',
       description: 'Create and develop IoT services/devices for your needs'
    },
    {
+      icon: <SiTestinglibrary />,
       id: '32e5582f4d',
       title: 'Software Testing',
-      description: 'Test your application with manual or automation testing using selenium and katalon'
+      description: 'Test your application with manual or automation testing'
    },
 ]
 
