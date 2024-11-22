9b8fab7eb9c424b87e16cad091ab11a53c8087b6
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-22 23:30:02 +0700

    feat<>: create layout styling on musicBox container

--- src/components/layouts/MusicBox.tsx
+++ src/components/layouts/MusicBox.tsx
@@ -1,6 +1,7 @@
 import { musicPlaylist } from '@/common/constants/music';
 import { cn } from '@/common/lib/utils';
 import React, { useState } from 'react'
+import Frequency from './audio-player/Frequency';
 
 const MusicBox = () => {
    const [isHover, setIsHover] = useState(false);
@@ -9,13 +10,11 @@ const MusicBox = () => {
       <div
          onMouseEnter={() => setIsHover(true)}
          onMouseLeave={() => setIsHover(false)}
-         className="fixed mt-5 top-0 right-0 z-30 hidden bg-[#EFECEC] dark:bg-calmBlue w-16 flex-col items-center justify-between overflow-hidden rounded-l-md border-r-4 border-black px-4 py-4 shadow-[0_3px_10px_rgb(0,0,0,0.2)] transition-all duration-500 hover:w-64 hover:items-stretch hover:p-4 hover:transition-all hover:duration-500 dark:shadow-neutral-800 lg:flex"
+         className="fixed mt-5 top-0 right-0 z-30 hidden bg-[#EFECEC] dark:bg-calmBlue w-16 flex-col items-center justify-between overflow-hidden rounded-l-md border-l-4 border-b-8 border-t-2 border-black px-4 py-4 transition-all duration-500 hover:w-64 hover:items-stretch hover:p-4 hover:transition-all hover:duration-500  lg:flex"
       >
          <div className={`flex h-64 ${isHover ? 'items-start' : 'items-center'}`}>
             <div className={cn(isHover ? 'block' : 'hidden')}>
-               <audio src={musicPlaylist[0].music}  controls
-               className="mb-20"
-               autoPlay/>
+               <Frequency />
             </div>
          </div>
       </div>
