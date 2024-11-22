d72fb597195193f2f2fa3ba3db99a0fbc64cc557
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-22 23:29:11 +0700

    feat<>: change object propperty name

--- src/common/constants/music.ts
+++ src/common/constants/music.ts
@@ -3,7 +3,7 @@
 export const musicPlaylist = [
    {
       musicCover: '',
-      music:   '/music/Die with a Smile.mp3',
+      src:   '/music/Die with a Smile.mp3',
       artist: 'Bruno Mars ft Lady Gaga',
       title: 'Die with a Smile'
    }
