abe02af2886ba51cfc6f49a938a5303e304402f6
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-23 22:13:50 +0700

    style<>: change bg color

--- src/modules/home/components/ServicesCard.tsx
+++ src/modules/home/components/ServicesCard.tsx
@@ -10,7 +10,7 @@ interface ServicesCardProps {
 
 const ServicesCard = ({ icon, id, title, description }: ServicesCardProps) => {
    return (
-      <BentoCard key={id} className='flex items-center justify-center gap-5 border border-black dark:border-slate-200 hover:bg-slate-300 dark:hover:bg-cyan-500 hover:scale-105 hover:shadow-light dark:hover:shadow-[0px, 20px, 20px, 5px, #000] hover:-translate-y-boxShadowX duration-300'>
+      <BentoCard key={id} className='flex items-center justify-center gap-5 border border-black dark:border-slate-200 hover:bg-slate-200 dark:hover:bg-cyan-500 hover:scale-105 hover:shadow-light dark:hover:shadow-[0px, 20px, 20px, 5px, #000] hover:-translate-y-boxShadowX duration-300'>
          <div className='text-5xl'>
             {icon}
          </div>
