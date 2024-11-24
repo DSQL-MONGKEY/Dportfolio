f54a111bcd6108f0491087e871fa35e8644bc2dc
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-24 21:40:26 +0700

    style<>: change height wrapper container

--- src/components/layouts/MusicBox.tsx
+++ src/components/layouts/MusicBox.tsx
@@ -1,7 +1,6 @@
 import { musicPlaylist } from '@/common/constants/music';
 import { cn } from '@/common/lib/utils';
 import React, { useState } from 'react'
-import Frequency from './audio-player/Frequency';
 import AudioPlayer from './audio-player/AudioPlayer';
 
 const MusicBox = () => {
@@ -11,9 +10,9 @@ const MusicBox = () => {
       <div
          onMouseEnter={() => setIsHover(true)}
          onMouseLeave={() => setIsHover(false)}
-         className="fixed mt-5 top-0 right-0 z-30 hidden bg-[#EFECEC] dark:bg-calmBlue w-16 flex-col items-center justify-between overflow-hidden rounded-l-md border-l-4 border-b-8 border-t-2 border-black px-4 py-4 transition-all duration-500 hover:w-64 hover:items-stretch hover:p-4 hover:transition-all hover:duration-500  lg:flex"
+         className="fixed mt-5 top-0 right-0 z-30 hidden bg-[#EFECEC] dark:bg-calmBlue w-16 flex-col items-center justify-between overflow-hidden rounded-l-md border-l-4 border-b-8 border-t-2 border-black px-4 py-4 transition-all duration-500 hover:w-64 hover:items-stretch hover:p-4 hover:transition-all hover:duration-500 lg:flex"
       >
-         <div className={`flex h-64 ${isHover ? 'items-start' : 'items-center'}`}>
+         <div className={`flex h-72 ${isHover ? 'items-start' : 'items-center'}`}>
             <div className={cn(isHover ? 'block' : 'hidden')}>
                <AudioPlayer isHover={isHover} />
             </div>
