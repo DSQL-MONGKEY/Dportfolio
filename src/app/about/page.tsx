25f04eb1c1567d2579412c4437ddeb8e892eb1e8
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-24 21:41:01 +0700

    feat<>: initial create

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 import Container from "@/components/elements/Container";
 import About from "@/modules/about";
 
-export default function Home() {
+export default function AboutPage() {
 	return (
 		<Container data-aos="fade-left">
 			<About />
