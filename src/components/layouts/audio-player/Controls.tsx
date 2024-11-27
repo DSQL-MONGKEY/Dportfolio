183b158717510d1d5e1cf85709192c12d41853ff
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-27 20:53:55 +0700

    feat<>: add progress-bar and ControlProps to progress utility

--- src/components/layouts/audio-player/Controls.tsx
+++ src/components/layouts/audio-player/Controls.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { Progress } from '@/components/ui/Progress';
+import React, { ReactNode } from 'react'
 
 import {    LiaPlayCircle } from "react-icons/lia";
 import { MdMotionPhotosPause } from "react-icons/md";
@@ -11,40 +12,57 @@ interface ControlsProps {
       handlePlayPause: () => void
    }
    isPlaying: boolean
+   progress: number
+   formatTime: (time: number) => ReactNode
+   currentTime: number
+   duration: number
 }
 
-const Controls = ({ onClick, isPlaying }: ControlsProps) => {
+const Controls = ({ onClick, isPlaying, progress, formatTime, currentTime, duration }: ControlsProps) => {
    const { handlePrevTrack, handleNextTrack, handlePlayPause } = onClick;
 
    return (
-      <div className="flex justify-between">
-         {/* previous song button */}
-         <button 
-            className="text-2xl"
-            onClick={handlePrevTrack}>
-            <SlControlRewind/>
-         </button>
-         
-         {/* play/pause button */}
-         <button 
-            className="text-2xl"
-            onClick={handlePlayPause}>
-            {isPlaying ? (
-               <MdMotionPhotosPause/>
-            ) : (
-               <LiaPlayCircle />
-            )
-            }
-         </button>
-         
-         {/* next song button */}
-         <button
-            className="text-2xl"
-            onClick={handleNextTrack}
-         >
-            <SlControlRewind className="rotate-180" />
-         </button>
-      </div>
+      <>
+         <div className="w-52">
+               <Progress 
+                  value={progress}
+                  className="w-52"
+               />
+               <div className="flex justify-between text-sm text-muted-foreground">
+                  <span>{formatTime(currentTime)}</span>
+                  <span>{formatTime(duration)}</span>
+               </div>
+            </div>
+
+         <div className="flex justify-between">
+            {/* previous song button */}
+            <button 
+               className="text-2xl"
+               onClick={handlePrevTrack}>
+               <SlControlRewind/>
+            </button>
+            
+            {/* play/pause button */}
+            <button 
+               className="text-2xl"
+               onClick={handlePlayPause}>
+               {isPlaying ? (
+                  <MdMotionPhotosPause/>
+               ) : (
+                  <LiaPlayCircle />
+               )
+               }
+            </button>
+            
+            {/* next song button */}
+            <button
+               className="text-2xl"
+               onClick={handleNextTrack}
+            >
+               <SlControlRewind className="rotate-180" />
+            </button>
+         </div>
+      </>
    )
 }
 
