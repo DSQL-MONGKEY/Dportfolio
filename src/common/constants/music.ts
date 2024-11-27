3c34290418a3ce1b7ed0a0d481e02bb54ed14279
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-27 20:43:58 +0700

    feat<>: add new music item list

--- src/common/constants/music.ts
+++ src/common/constants/music.ts
@@ -6,5 +6,11 @@ export const musicPlaylist = [
       src:   '/music/Die with a Smile.mp3',
       artist: 'Bruno Mars ft Lady Gaga',
       title: 'Die with a Smile'
-   }
+   },
+   {
+      musicCover: '',
+      src:   '/music/Runtuh.mp3',
+      artist: 'Feby Putri ft Fiersa Besari',
+      title: 'Runtuh',
+   },
 ]
