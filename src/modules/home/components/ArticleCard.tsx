8cd4f4222ef6ea45013ba5d5c8a9dcfc5a403e86
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-30 20:43:02 +0700

    feat<>: half create rendered card content

--- src/modules/home/components/ArticleCard.tsx
+++ src/modules/home/components/ArticleCard.tsx
@@ -1,10 +1,20 @@
+import { ArticleItem } from '@/common/types/article'
 import React from 'react'
 
-const ArticleCard = () => {
+interface ArticleCardProps {
+   index: number
+   data: ArticleItem
+}
+
+const ArticleCard = ({ index, data }: ArticleCardProps) => {
+
    return (
-      <button className='relative transition hover:scale-95 hover:duration-300'>
+      <button key={index} className='relative transition hover:scale-95 hover:duration-300'>
          <div className="relative z-10 flex h-max min-w-[250px] flex-col items-start space-y-1">
-            
+            <div className="overflow-hidden h-28 w-full rounded-md">
+            </div>
+            <p>{data.title}</p>
+            <p>{data.tags}</p>
          </div>
       </button>
    )
