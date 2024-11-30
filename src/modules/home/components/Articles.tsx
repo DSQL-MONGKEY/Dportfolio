203c8e2a975b5b6fadc2b50251569fc11e51f505
Dimpfe <dimasprasetyo7696@gmail.com>
2024-11-30 20:44:17 +0700

    feat<>: add card conditional rendering

--- src/modules/home/components/Articles.tsx
+++ src/modules/home/components/Articles.tsx
@@ -2,8 +2,12 @@ import SectionHeading from '@/components/elements/SectionHeading';
 import SectionSubHeading from '@/components/elements/SectionSubHeading';
 import React from 'react'
 import { RxCrumpledPaper } from "react-icons/rx";
+import ArticleCard from './ArticleCard';
+import { articles } from '@/common/constants/constants';
+import ArticleLoading from './ArticleLoading';
 
 const Articles = () => {
+   
    return (
       <section>
          <div className="space-y-2">
@@ -14,7 +18,12 @@ const Articles = () => {
          </div>
 
          <div className='no-scrollbar mt-4 flex h-40 flex-row space-x-3 overflow-y-hidden overflow-x-scroll pt-2'>
-            
+            {true ? 
+               [1, 2, 3, 4].map(item => <ArticleLoading key={item} />) :
+               
+               articles.map((data, idx) => (
+               <ArticleCard key={idx} data={data}  index={idx}/>
+            ))}
          </div>
       </section>
    )
