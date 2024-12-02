e02c67d4a4fe3f18826b7b6e2153100a8e482a26
Dimpfe <dimasprasetyo7696@gmail.com>
2024-12-02 16:27:36 +0700

    feat<>: create journeys page

---
+++ src/app/journeys/page.tsx
@@ -0,0 +1,13 @@
+import Container from '@/components/elements/Container'
+import Journeys from '@/modules/journey'
+import React from 'react'
+
+const JourneysPage = () => {
+   return (
+      <Container data-aos="fade-left">
+         <Journeys />
+      </Container>
+   )
+}
+
+export default JourneysPage
