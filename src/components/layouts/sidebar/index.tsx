be0f86670b3003458437a5efe78f805081e83fbc
Dimpfe <dimasprasetyo7696@gmail.com>
2024-12-07 22:10:20 +0700

    chore<>: remove blank space

--- src/components/layouts/sidebar/index.tsx
+++ src/components/layouts/sidebar/index.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 import Menu from './Menu'
 
 const Sidebar = () => {
-  	return (
+	return (
 		<div className="sticky top-0 z-10 flex flex-col transition-all duration-300 lg:py-8">
 			<nav className="hidden md:block">
 				<div className="hidden lg:block">
