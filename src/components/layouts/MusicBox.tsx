0a75f15f7e9e8a04be0bb51ba1ddf73a145bd3f3
Dimpfe <dimasprasetyo7696@gmail.com>
2024-12-07 22:08:47 +0700

    style<>: add height container to 80

--- src/components/layouts/MusicBox.tsx
+++ src/components/layouts/MusicBox.tsx
@@ -10,7 +10,7 @@ const MusicBox = () => {
          onMouseLeave={() => setIsHover(false)}
          className="fixed mt-5 top-0 right-0 z-30 hidden bg-[#EFECEC] dark:bg-calmBlue w-16 flex-col items-center justify-between overflow-hidden rounded-l-md border-l-4 border-b-8 border-t-2 border-black px-4 py-4 transition-all duration-500 hover:w-64 hover:items-stretch hover:p-4 hover:transition-all hover:duration-500 lg:flex"
       >
-         <div className={`flex h-72 ${isHover ? 'items-start' : 'items-center'}`}>
+         <div className={`flex h-80 ${isHover ? 'items-start' : 'items-center'}`}>
                <AudioPlayer isHover={isHover} />
          </div>
       </div>
