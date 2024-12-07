879bbab7cbfc2df474a6ee9bc0118e85b09e8e85
Dimpfe <dimasprasetyo7696@gmail.com>
2024-12-07 22:07:27 +0700

    feat<>: change music title

--- src/common/constants/music.ts
+++ src/common/constants/music.ts
@@ -5,18 +5,18 @@ export const musicPlaylist = [
       musicCover: '',
       src:   '/music/Die with a Smile.mp3',
       artist: 'Bruno Mars ft Lady Gaga',
-      title: 'Die with a Smile'
+      title: 'Die with a Smile - Bruno Mars ft Lady Gaga'
    },
    {
       musicCover: '',
       src:   '/music/Runtuh.mp3',
       artist: 'Feby Putri ft Fiersa Besari',
-      title: 'Runtuh',
+      title: 'Runtuh - Feby Putri ft Fiersa Besari',
    },
    {
       musicCover: '',
       src:   '/music/JVKE - golden hour.mp3',
       artist: 'JVKE',
-      title: 'Golden Hour',
+      title: 'Golden Hour - JVKE',
    },
 ]
