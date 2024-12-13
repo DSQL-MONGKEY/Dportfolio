df947ffed6a2f5cea4b5226b578292eae52ef4a2
Dimpfe <dimasprasetyo7696@gmail.com>
2024-12-14 05:57:00 +0700

    feat<>: add ItemContainer to be render

--- src/modules/journey/components/Timeline.tsx
+++ src/modules/journey/components/Timeline.tsx
@@ -1,14 +1,25 @@
 "use client"
 
 import React from 'react'
-import ItemJourney from './ItemJourney';
+import ItemContainer from './ItemContainer'
+import { journeys } from '@/common/constants/constants'
 
 
 const Timeline = () => {
 
    return (
       <section>
-         <ItemJourney />
+         <div className='flex flex-col gap-5'>
+            {journeys.map((data, idx) => (
+               <ItemContainer
+                  key={idx}
+                  title={data.title}
+                  role={data.role}
+                  date={data.date}
+                  className={idx % 2 == 0 ? 'flex-row-reverse' : 'flex-row'}
+               />
+            ))}
+         </div>
       </section>
    )
 }
