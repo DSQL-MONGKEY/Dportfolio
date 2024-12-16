fa4c84ae63cb4e0d3e2613a16d94d1d228445529
Dimpfe <dimasprasetyo7696@gmail.com>
2024-12-16 07:47:02 +0700

    feat<>: creation on progress

--- src/components/layouts/MobileSlideNav.tsx
+++ src/components/layouts/MobileSlideNav.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect } from 'react'
+import { motion } from 'framer-motion';
+import Menu from './sidebar/Menu';
+import { navLinks } from '@/common/constants/constants';
+import useIsMobile from '@/hooks/useIsMobile';
+import { useMenu } from '@/stores/menu';
+
 
 const MobileSlideNav = () => {
+   const isMobile = useIsMobile();
+   const { isOpen, toggleMenu } = useMenu();
+
+   useEffect(() => {
+      if(isOpen) {
+         document.body.style.overflow = 'hidden'
+      } else {
+         document.body.style.overflow = 'auto'
+      }
+
+      return () => {
+         document.body.style.overflow = 'auto'
+      }
+   }, [isOpen]);
+
    return (
-      <div>MobileSlideNav</div>
+      <div></div>
    )
 }
 
