00933dc99eed6e30b5003f03ec5326a17de4616d
Dimpfe <dimasprasetyo7696@gmail.com>
2024-12-29 19:48:57 +0700

    style, docs<>: change spacing y axis and add documentation

--- src/components/layouts/sidebar/Menu.tsx
+++ src/components/layouts/sidebar/Menu.tsx
@@ -11,12 +11,16 @@ interface MenuProps {
 
 const Menu = ({ title, list }: MenuProps) => {
    return (
-      <div className="flex flex-col space-y-1 lg:p-0">
+      <div className="flex flex-col space-y-2 lg:p-0">
+
+         {/* Render option when list have undefined */}
          {title && (
-            <div className="mb-2 ml-2 mt-1 hidden text-sm text-neutral-600 dark:text-neutral-500 lg:block">
+            <div className="mb-2 ml-2 mt-1 hidden text-sm text-neutral-600  dark:text-neutral-500 lg:block">
                {title}
             </div>
          )}
+
+         {/* Optional Chaining for mitigate empty/undefine values from the list */}
          {list?.map((item: MenuItemProps, index: number) => <MenuItem key={index} {...item} />)}
       </div>
    )
