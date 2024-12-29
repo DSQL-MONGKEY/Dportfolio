98745d52619c42a327007a54a95108542aae8f4d
Dimpfe <dimasprasetyo7696@gmail.com>
2024-12-29 19:49:31 +0700

    feat<>: add isMobile conditional rendering style

--- src/components/layouts/sidebar/MenuItem.tsx
+++ src/components/layouts/sidebar/MenuItem.tsx
@@ -5,10 +5,13 @@ import { usePathname } from 'next/navigation';
 
 import Link from 'next/link';
 import React, { useState } from 'react'
+import useIsMobile from '@/hooks/useIsMobile';
+import clsx from 'clsx';
 
 const MenuItem = ({ href, title, icon, className = '', children, isHover, onClick }: MenuItemProps) => {
    const [isHovered, setIsHovered] = useState(false);
    const { hideMenu } = useMenu();
+   const isMobile = useIsMobile();
    const isExternalUrl = href?.includes('http')
    const pathname = usePathname()
   const url = new URL(href, 'http://localhost:3000')
@@ -20,8 +23,8 @@ const MenuItem = ({ href, title, icon, className = '', children, isHover, onClic
       gap-2 text-neutral-900 font-poppins dark:text-neutral-300 hover:text-neutral-950 hover:dark:text-white transition-all ease-out
    ${
       pathname === url.pathname
-      ? 'bg-[#8ad451] dark:bg-[#2192FF] dark:!text-white border-2 border-black shadow-light dark:shadow-dark'
-      : 'border-2 border-slate-300 hover:border-black dark:border-slate-200 hover:dark:!border-black hover:shadow-light hover:dark:lg:bg-[#2192FF] hover:lg:bg-neutral-100 hover:lg:rounded-lg lg:hover:gap-3 lg:transition-all lg:duration-300'
+      ? 'bg-[#8ad451] dark:bg-[#2192FF] dark:!text-white border-4 border-black shadow-light dark:shadow-dark'
+      : clsx('border-4 hover:border-black dark:border-slate-200 hover:dark:!border-black hover:shadow-light hover:dark:lg:bg-[#2192FF] hover:lg:bg-neutral-100 hover:lg:rounded-lg lg:hover:gap-3 lg:transition-all lg:duration-300', isMobile ? 'border-neutral-900 dark:border-black dark:border-4 dark:text-black hover:dark:!border-neutral-900 dark:bg-[#C3C0F3] bg-slate-200' : 'border-slate-300')
    }`
 
    
