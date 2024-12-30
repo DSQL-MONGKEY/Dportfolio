97b319156fa3c2876f13801716029d6a6f0556da
Dimpfe <dimasprasetyo7696@gmail.com>
2024-12-30 13:21:15 +0700

    feat<>: change themeToggle button without button component

--- src/components/elements/ThemeToggleIcon.tsx
+++ src/components/elements/ThemeToggleIcon.tsx
@@ -5,7 +5,6 @@ import { useTheme } from 'next-themes'
 import { motion } from 'framer-motion';
 import { IoIosCloudyNight } from "react-icons/io";
 import { IoPartlySunnySharp } from "react-icons/io5";
-import { Button } from '../ui/Button';
 
 const ThemeToggleIcon = () => {
    const { setTheme, resolvedTheme } = useTheme();
@@ -18,15 +17,14 @@ const ThemeToggleIcon = () => {
       <motion.div
          initial={{ opacity: 0, scale: 0.5 }}
          animate={{ opacity: 1, scale: 1 }}
-         transition={{ duration: 0.5, delay: 0.5 }}
+         transition={{ duration: 0.5, delay: 0.3 }}
          className='p-1'
       >
-         <Button 
-            variant={'default'}
+         <button
             onClick={toggleTheme}
-         >
-            {resolvedTheme === 'dark' ? <IoPartlySunnySharp /> : <IoIosCloudyNight />}
-         </Button>
+            className='p-2 rounded-md bg-neutral-200 dark:bg-main dark:text-black shadow-light border-2 border-black hover:scale-110 active:scale-90 transition-all duration-300'>
+               {resolvedTheme === 'dark' ? <IoPartlySunnySharp /> : <IoIosCloudyNight />}
+         </button>
       </motion.div>
    )
 }
