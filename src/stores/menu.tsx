c2bf51e72877be768d2f7b8963c55e2003dd700f
Dimpfe <dimasprasetyo7696@gmail.com>
2024-12-31 05:32:36 +0700

    feat<>: change interface name

--- src/stores/menu.tsx
+++ src/stores/menu.tsx
@@ -1,15 +1,15 @@
 import { create } from 'zustand';
 
-export interface InitialModalState {
+export interface InitialMenuState {
    isOpen: boolean,
 }
-export interface InitialModalAction {
+export interface InitialMenuAction {
    showMenu(): void,
    hideMenu(): void,
    toggleMenu(): void
 }
 
-export const useMenu = create<InitialModalState & InitialModalAction>()(set => ({
+export const useMenu = create<InitialMenuState & InitialMenuAction>()(set => ({
    isOpen: false,
    showMenu: () => set({ isOpen: true }),
    hideMenu: () => set({ isOpen: false }),
