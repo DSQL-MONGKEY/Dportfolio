5133f717087e8f827858b7a896c45bc11b392cd1
Dimpfe <dimasprasetyo7696@gmail.com>
2024-12-31 18:44:57 +0700

    feat<>: add mobile conditional rendering for audioplayer

--- src/components/layouts/MusicBox.tsx
+++ src/components/layouts/MusicBox.tsx
@@ -2,9 +2,11 @@
 
 import React, { useState } from 'react'
 import AudioPlayer from './audio-player/AudioPlayer';
+import useIsMobile from '@/hooks/useIsMobile';
 
 const MusicBox = () => {
    const [isHover, setIsHover] = useState(false);
+   const isMobile = useIsMobile();
 
    return (
       <div
@@ -13,7 +15,7 @@ const MusicBox = () => {
          className="fixed mt-5 top-0 right-0 z-30 hidden bg-[#EFECEC] dark:bg-calmBlue w-16 flex-col items-center justify-between overflow-hidden rounded-l-md border-l-4 border-b-8 border-t-2 border-black px-4 py-4 transition-all duration-500 hover:w-64 hover:items-stretch hover:p-4 hover:transition-all hover:duration-500 lg:flex"
       >
          <div className={`flex h-80 ${isHover ? 'items-start' : 'items-center'}`}>
-               <AudioPlayer isHover={isHover} />
+               {!isMobile && <AudioPlayer isHover={isHover} isMobile={isMobile} />}
          </div>
       </div>
    )
