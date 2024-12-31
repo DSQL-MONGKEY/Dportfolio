2281641f6d6497df83e02d6a380ba48d38b82d31
Dimpfe <dimasprasetyo7696@gmail.com>
2024-12-31 18:49:10 +0700

    feat<>: add audioPlayer component to be render and add isOpen props for style conditional

--- src/components/layouts/sidebar/MobileMenu.tsx
+++ src/components/layouts/sidebar/MobileMenu.tsx
@@ -3,17 +3,22 @@ import { motion } from 'framer-motion';
 import Menu from './Menu';
 import { navLinks } from '@/common/constants/constants';
 import Breakline from '@/components/elements/Breakline';
+import AudioPlayer from '../audio-player/AudioPlayer';
 
-const MobileMenu = () => {
+interface MobileMenuProps {
+   isOpen: boolean
+}
+
+const MobileMenu = ({ isOpen }: MobileMenuProps) => {
    return (
       <motion.div
-         initial={{ x: -100 }}
-         animate={{ opacity: 1, x: 0 }}
+         animate={{ opacity: 1, x:0}}
          transition={{ duration: 0.3 }}
-         className='my-3 flex flex-col h-screen'
-      >
-            <Breakline className='mt-2' />
-            <Menu list={navLinks} />
+         className={`${isOpen ? 'block' : 'hidden'} my-3 flex flex-col h-screen transition-all duration-300`}
+      > 
+         <AudioPlayer isMobile={true} />
+         <Breakline className='mt-2' />
+         <Menu list={navLinks} />
       </motion.div>
    )
 }
