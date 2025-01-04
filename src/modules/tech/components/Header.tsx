26e1b0272e18f04065c734db48e98ab740a6b29a
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-04 23:38:15 +0700

    feat<>: create  header component

--- src/modules/tech/components/Header.tsx
+++ src/modules/tech/components/Header.tsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Header = () => {
    return (
-      <div>Header</div>
+      <div className="flex flex-col gap-4  justify-center items-center p-5 bg-neutral-200 shadow-light dark:shadow-dark rounded-lg border-2 border-black">
+         <div>
+            <h1 className="font-paytone text-4xl sm:text-5xl md:text-6xl text-neutral-700 dark:text-neutral-900">
+               Techy and tools
+            </h1>
+         </div>
+         <div>
+            <p className="font-outfit font-bold text-lg text-neutral-700 dark:text-neutral-500">
+               That I used to built your request
+            </p>
+         </div>
+      </div>
    )
 }
 
