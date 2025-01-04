a3196b3d46d1de91ba78e2934dbd2f76864535a0
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-04 23:38:25 +0700

    feat<>: add breakline

--- src/modules/tech/components/Tech.tsx
+++ src/modules/tech/components/Tech.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Header'
+import Breakline from '@/components/elements/Breakline'
 
 const Tech = () => {
    return (
       <>
          <Header />
+         <Breakline />
       </>
    )
 }
