4a1b7636a585153aab717315b7cf0699719e90a6
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-05 23:33:39 +0700

    feat<>: add board to be render

--- src/modules/tech/components/Tech.tsx
+++ src/modules/tech/components/Tech.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Header'
 import Breakline from '@/components/elements/Breakline'
+import Board from './Board'
 
 const Tech = () => {
    return (
       <>
          <Header />
          <Breakline />
+         <Board />
       </>
    )
 }
