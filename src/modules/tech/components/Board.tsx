5732fe0dad16783a558fa67acf1d69ff21fea0b7
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-05 23:32:58 +0700

    feat<>: on progress

--- src/modules/tech/components/Board.tsx
+++ src/modules/tech/components/Board.tsx
@@ -1,8 +1,25 @@
+import { skillSet } from '@/common/constants/constants'
 import React from 'react'
+import Card from './Card'
 
 const Board = () => {
+
+
    return (
-      <div>Board</div>
+      <div className="p-2 border-2 border-black rounded-lg">
+         <div className="flex flex-row flex-wrap justify-center gap-3 ">
+            {skillSet.map((skill, index) => (
+               <Card 
+                  key={index} 
+                  title={skill.title} 
+                  id={index} 
+                  label={skill.label} 
+                  icon={skill.icon} 
+                  level={skill.level} 
+               />
+            ))}
+         </div>
+      </div>
    )
 }
 
