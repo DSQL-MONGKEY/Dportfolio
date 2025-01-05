b8fee6c574626448f969e00c1c045f88f60fc154
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-05 23:33:26 +0700

    style<>: add style when hovered

--- src/modules/tech/components/Header.tsx
+++ src/modules/tech/components/Header.tsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 const Header = () => {
    return (
-      <div className="flex flex-col gap-4  justify-center items-center p-5 bg-neutral-200 shadow-light dark:shadow-dark rounded-lg border-2 border-black">
-         <div>
-            <h1 className="font-paytone text-4xl sm:text-5xl md:text-6xl text-neutral-700 dark:text-neutral-900">
-               Techy and tools
-            </h1>
-         </div>
-         <div>
-            <p className="font-outfit font-bold text-lg text-neutral-700 dark:text-neutral-500">
-               That I used to built your request
-            </p>
+      <div className="bg-black rounded-lg">
+         <div className="flex flex-col gap-4 relative justify-center items-center p-5 bg-neutral-200 -translate-y-[8px] -translate-x-[8px] hover:translate-y-[0px] hover:translate-x-[0px] transition-all ease-in-out duration-200 rounded-lg border-2 border-black select-none">
+            <div>
+               <h1 className="font-paytone text-4xl sm:text-5xl md:text-6xl text-neutral-700 dark:text-neutral-900">
+                  Techy and Tools
+               </h1>
+            </div>
+            <div>
+               <p className="font-outfit font-bold text-lg text-neutral-700 dark:text-neutral-500">
+                  That I used to built your request
+               </p>
+            </div>
          </div>
       </div>
    )
