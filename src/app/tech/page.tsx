5596ce6d24c3948d4e15c0bd1b91f0537dec155e
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-19 21:38:45 +0700

    feat<>: initial create

---
+++ src/app/tech/page.tsx
@@ -0,0 +1,13 @@
+import Container from '@/components/elements/Container'
+import Tech from '@/modules/tech'
+import React from 'react'
+
+const TechPage = () => {
+   return (
+      <Container data-aos="fade-left">
+         <Tech />
+      </Container>
+   )
+}
+
+export default TechPage
