ac21061cc492999f994fc5799ca402ff0429a254
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-24 22:36:18 +0700

    feat<>: add quest component to be render

--- src/modules/tech/components/Tech.tsx
+++ src/modules/tech/components/Tech.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './Header'
 import Breakline from '@/components/elements/Breakline'
 import Board from './Board'
+import Quest from './Quest'
 
 const Tech = () => {
    return (
@@ -9,6 +10,8 @@ const Tech = () => {
          <Header />
          <Breakline />
          <Board />
+         <Breakline />
+         <Quest />
       </>
    )
 }
