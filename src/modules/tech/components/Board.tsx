37c717c9fd956d64022ed52de50e33a6b0e7f297
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-24 22:35:42 +0700

    style<>: change justify items

--- src/modules/tech/components/Board.tsx
+++ src/modules/tech/components/Board.tsx
@@ -7,7 +7,7 @@ const Board = () => {
 
    return (
       <div className="p-2 border-2 border-black rounded-lg">
-         <div className="flex flex-row flex-wrap justify-center gap-3 ">
+         <div className="flex flex-row flex-wrap justify-evenly gap-3 ">
             {skillSet.map((skill, index) => (
                <Card 
                   key={index} 
