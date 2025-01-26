aecbe5673daf599b4c6f1e605d90cebf78a52dd4
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-26 23:12:02 +0700

    feat<>: initiate store hook

--- src/stores/mini-games.tsx
+++ src/stores/mini-games.tsx
@@ -1,3 +1,5 @@
+import { create } from "zustand";
+
 export interface InitialMiniGamesState {
    isPlaying: boolean,
    isGameOver: boolean,
@@ -13,3 +15,16 @@ export interface InitialMiniGamesAction {
    setIsDeadTime: (isDeadTime: number) => void,
    setProgress: (progress: number) => void,
 }
+
+export const useMiniGames = create<InitialMiniGamesState & InitialMiniGamesAction>()(set => ({
+   isPlaying: false,
+   isGameOver: false,
+   isComplete: false,
+   isDeadTime: 0,
+   progress: 0,
+   setIsPlaying: () => set(prev => ({ isPlaying: !prev.isPlaying })),
+   setIsGameOver: (isGameOver) => set({ isGameOver }),
+   setIsComplete: (isComplete) => set({ isComplete }),
+   setIsDeadTime: (isDeadTime) => set({ isDeadTime }),
+   setProgress: (progress) => set({ progress }),
+}))
