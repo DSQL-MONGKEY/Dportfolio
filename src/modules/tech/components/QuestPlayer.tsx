ed424ee831907f25bd7993ccb64c65d2dd4581f9
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-26 23:11:29 +0700

    feat<>: add category picker

--- src/modules/tech/components/QuestPlayer.tsx
+++ src/modules/tech/components/QuestPlayer.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+"use client"
+
+import { useMiniGames } from '@/stores/mini-games';
+import React, { useState } from 'react'
+import QuestPickCategory from './QuestPickCategory';
 
 const QuestPlayer = () => {
+   const questionCategory = ['tech', 'general', 'history', 'riddles']
+   const [category, setCategory] = useState<string>('');
+   const {
+      isPlaying,
+   } = useMiniGames();
+   
+
    return (
-      <div>QuestPlayer</div>
+      <div className="mt-2 flex flex-col justify-between p-3 w-full h-[450px] bg-neutral-100 dark:bg-zinc-800 border-2 border-black shadow-light rounded-md">
+         <div className="">
+            <h2 className="text-2xl font-paytone">
+               Silakan pilih kategori pertanyaan yang kamu inginkan...
+            </h2>
+         </div>
+         <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 justify-center">
+            {!isPlaying && (
+               <QuestPickCategory
+                  category={category}
+                  setCategory={setCategory}
+                  categories={questionCategory}
+               />
+            )}
+         </div>
+      </div>
    )
 }
 
