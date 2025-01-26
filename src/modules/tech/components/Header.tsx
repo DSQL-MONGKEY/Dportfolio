785d06f81c218d5ed90cfb9f76a13611de0e085d
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-26 21:50:13 +0700

    style<>: change bg color

--- src/modules/tech/components/Header.tsx
+++ src/modules/tech/components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
    return (
       <div className="bg-black rounded-lg">
-         <div className="flex flex-col gap-4 relative justify-center items-center p-5 bg-neutral-200 -translate-y-[8px] -translate-x-[8px] hover:translate-y-[0px] hover:translate-x-[0px] transition-all ease-in-out duration-200 rounded-lg border-2 border-black select-none">
+         <div className="flex flex-col gap-4 relative justify-center items-center p-5 bg-neutral-100 -translate-y-[8px] -translate-x-[8px] hover:translate-y-[0px] hover:translate-x-[0px] transition-all ease-in-out duration-200 rounded-lg border-2 border-black select-none">
             <div>
                <h1 className="font-paytone text-4xl sm:text-5xl md:text-6xl text-neutral-700 dark:text-neutral-900">
                   Techy and Tools
