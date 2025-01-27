2dc927c1ac59161e6faf59b9a9cbecdec8cacde1
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-27 14:03:47 +0700

    feat<>: add logic for randomize questions and create ui component to display the question

--- src/modules/tech/components/Units.tsx
+++ src/modules/tech/components/Units.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Units = () => {
+interface ChoicesTypes {
+   answer: string
+   isCorrect: boolean
+}
+
+export interface UnitsProps  {
+   categoryData: Array<{
+      type: string
+      question: string
+      choices: ChoicesTypes[],
+      clue?: string
+      solution?: string
+   }>
+}
+
+const Units = ({ categoryData }: UnitsProps) => {
+   const [shuffledIndexes, setShuffledIndexes] = useState(() => categoryData.map((_, idx) => idx).sort(() => Math.random() - 0.5));
+
+   const currentIndex = shuffledIndexes[0];
+   const handleAnswer = (isCorrect: boolean) => {
+      alert(isCorrect ? "Jawaban benar!" : "Jawaban salah!");
+      setShuffledIndexes(prev => prev.slice(1));
+   }
+
+   const currentQuestion = categoryData[currentIndex];
+
+   if(shuffledIndexes.length === 0) {
+      return (
+         <div className="w-full h-full flex flex-grow justify-center items-center">
+            <h2 className="text-2xl font-paytone">
+               Permainan selesai! Terima kasih telah bermain.
+            </h2>
+         </div>
+      )
+   }
+
    return (
-      <div>Units</div>
+      <>
+         <div className="w-full h-full flex flex-grow justify-center items-center">
+            <h2 className="text-2xl font-paytone">
+               {currentQuestion.question}
+            </h2>
+         </div>
+         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 justify-center">
+            {currentQuestion.choices.map((data, idx) => (
+               <div key={idx} className="group bg-black rounded-md h-16 sm:h-24">
+               <button 
+                  onClick={() => handleAnswer(data.isCorrect)}
+                  className={`bg-slate-200 h-full border-2 w-full border-black rounded-md group-hover:-translate-x-[10px] group-hover:-translate-y-[10px]
+                  group-active:-translate-x-[0px] group-active:-translate-y-[0px]
+                  transition-all duration-100`}
+               >
+                  <span className={`text-neutral-800 text-xl sm:text-2xl font-paytone capitalize `}>
+                     {data.answer}
+                  </span>
+               </button>
+            </div>
+            ))}
+         </div>
+      </>
    )
 }
+
+// ${category == item ? 'bg-green-500 dark:bg-[#F5004F]' : 'bg-slate-200 dark:bg-[#FFC700]'}
+// ${category == item ? 'text-white': 'text-neutral-800'} 
 
 export default Units
