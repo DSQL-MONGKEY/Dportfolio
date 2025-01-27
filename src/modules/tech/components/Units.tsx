26f695b3744ee25cd6ee498f342e2a98525205c5
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-28 00:10:40 +0700

    feat<>: add sfx to button action and add use client tag

--- src/modules/tech/components/Units.tsx
+++ src/modules/tech/components/Units.tsx
@@ -1,4 +1,8 @@
+"use client"
+
+import { useQuestScore } from '@/stores/questScore';
 import React, { useState } from 'react'
+import useSound from 'use-sound';
 
 interface ChoicesTypes {
    answer: string
@@ -17,16 +21,48 @@ export interface UnitsProps  {
 
 const Units = ({ categoryData }: UnitsProps) => {
    const [shuffledIndexes, setShuffledIndexes] = useState(() => categoryData.map((_, idx) => idx).sort(() => Math.random() - 0.5));
-
    const currentIndex = shuffledIndexes[0];
+   const [win] = useSound('/music/sfx/win-sfx.mp3');
+   const [lose] = useSound('/music/sfx/lose-sfx.mp3');
+   const [correct] = useSound('/music/sfx/correct-sfx.mp3');
+   const [uncorrect] = useSound('/music/sfx/uncorrect-sfx.mp3');
+   const {
+      currentScore,
+      setCurrentScore,
+   } = useQuestScore();
+
+   const correctAnswer = () => {
+      correct();
+      setCurrentScore(currentScore + 10);
+   }
+   const uncorrectAnswer = () => {
+      uncorrect();
+   }
+
    const handleAnswer = (isCorrect: boolean) => {
       alert(isCorrect ? "Jawaban benar!" : "Jawaban salah!");
+      
       setShuffledIndexes(prev => prev.slice(1));
+      return isCorrect ? correctAnswer() : uncorrectAnswer();
+   }
+
+   const handleWinning = () => {
+      win();
+   }
+   const handleLosing = () => {
+      lose();
    }
 
    const currentQuestion = categoryData[currentIndex];
 
+   
+
    if(shuffledIndexes.length === 0) {
+      if(currentScore >=75) {
+         handleWinning();
+      } else {
+         handleLosing();
+      }
       return (
          <div className="w-full h-full flex flex-grow justify-center items-center">
             <h2 className="text-2xl font-paytone">
@@ -35,6 +71,8 @@ const Units = ({ categoryData }: UnitsProps) => {
          </div>
       )
    }
+
+   
 
    return (
       <>
@@ -46,7 +84,8 @@ const Units = ({ categoryData }: UnitsProps) => {
          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 justify-center">
             {currentQuestion.choices.map((data, idx) => (
                <div key={idx} className="group bg-black rounded-md h-16 sm:h-24">
-               <button 
+               <button
+                  id='button-answer'
                   onClick={() => handleAnswer(data.isCorrect)}
                   className={`bg-slate-200 h-full border-2 w-full border-black rounded-md group-hover:-translate-x-[10px] group-hover:-translate-y-[10px]
                   group-active:-translate-x-[0px] group-active:-translate-y-[0px]
