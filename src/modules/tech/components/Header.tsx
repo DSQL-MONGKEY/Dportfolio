7e08c4758a58938db937280b8affef59f1b91f18
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-28 16:01:56 +0700

    style<>: add darkmode color

--- src/modules/tech/components/Header.tsx
+++ src/modules/tech/components/Header.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Header = () => {
    return (
-      <div className="bg-black rounded-lg">
-         <div className="flex flex-col gap-4 relative justify-center items-center p-5 bg-neutral-100 -translate-y-[8px] -translate-x-[8px] hover:translate-y-[0px] hover:translate-x-[0px] transition-all ease-in-out duration-200 rounded-lg border-2 border-black select-none">
+      <div className="bg-black border-2 border-black dark:border-neutral-100 rounded-lg">
+         <div className="flex flex-col gap-4 relative justify-center items-center p-5 bg-neutral-100 dark:bg-zinc-800 -translate-y-[8px] -translate-x-[8px] hover:translate-y-[0px] hover:translate-x-[0px] transition-all ease-in-out duration-200 rounded-lg border-2 border-black dark:border-neutral-100 select-none">
             <div>
-               <h1 className="font-paytone text-4xl sm:text-5xl md:text-6xl text-neutral-700 dark:text-neutral-900">
+               <h1 className="font-paytone text-4xl sm:text-5xl md:text-6xl text-neutral-700 dark:text-neutral-100">
                   Techy and Tools
                </h1>
             </div>
             <div>
-               <p className="font-outfit font-bold text-lg text-neutral-700 dark:text-neutral-500">
+               <p className="font-outfit font-bold text-lg text-neutral-700 dark:text-neutral-300">
                   That I used to built your request
                </p>
             </div>
