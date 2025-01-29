472616c67979746902239c61ae70c0f308e61025
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-29 11:05:37 +0700

    feat<>: add ItemDetails component to wrap ItemContainer

--- src/modules/journey/components/Timeline.tsx
+++ src/modules/journey/components/Timeline.tsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import ItemContainer from './ItemContainer'
 import { journeys } from '@/common/constants/constants'
+import ItemDetails from './ItemDetails'
 
 
 const Timeline = () => {
@@ -11,13 +12,23 @@ const Timeline = () => {
       <section>
          <div className='flex flex-col gap-5'>
             {journeys.map((data, idx) => (
-               <ItemContainer
-                  key={idx}
-                  title={data.title}
+               <ItemDetails 
+                  key={idx} 
+                  title={data.title} 
+                  logo={data.logo}
                   role={data.role}
                   date={data.date}
-                  className={idx % 2 == 0 ? 'flex-row-reverse' : 'flex-row'}
-               />
+                  isPresent={data.isPresent}
+                  jobDesc={data.jobDesc}
+                  coworks={data.coworks}
+               >
+                     <ItemContainer
+                        title={data.title}
+                        role={data.role}
+                        date={data.date}
+                        className={idx % 2 == 0 ? 'flex-row-reverse' : 'flex-row'}
+                     />
+               </ItemDetails>
             ))}
          </div>
       </section>
