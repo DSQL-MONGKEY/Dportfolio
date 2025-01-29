6ba57869ea55292b0b7bb4e2a60a556d9ce41995
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-29 20:33:48 +0700

    style<>: adjust layout and color

--- src/modules/journey/components/Users.tsx
+++ src/modules/journey/components/Users.tsx
@@ -12,13 +12,13 @@ interface UsersProps {
 const Users = ({ link, name, tag, role, opinion }: UsersProps) => {
    return (
    <>
-      <div id='user-profile' className='flex flex-row w-full'>
+      <div id='user-profile' className='flex flex-row w-full md:bg-neutral-100 md:dark:bg-zinc-900 md:rounded-md md:border-2 md:border-black md:p-2 shadow-light'>
          <div className='flex flex-col'>
             <Link href={link!} className='space-x-2'>
                <span className='font-poppins text-lg'>
                   {name}
                </span>
-               <span className='font-poppins'>
+               <span className='font-poppins opacity-40'>
                   @{tag}
                </span>
             </Link>
@@ -27,8 +27,10 @@ const Users = ({ link, name, tag, role, opinion }: UsersProps) => {
             </div>
          </div>
       </div>
-      <div>
-         {opinion}
+      <div className='flex justify-center'>
+         <p>
+            {opinion}
+         </p>
       </div>
    </>
 )
