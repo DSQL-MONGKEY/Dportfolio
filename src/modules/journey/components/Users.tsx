8158a7bbb69b0db65f1f086a95868edef0192e55
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-29 11:59:37 +0700

    feat<>: create Users display data component

--- src/modules/journey/components/Users.tsx
+++ src/modules/journey/components/Users.tsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import Link from 'next/link';
 
-const Users = () => {
+interface UsersProps {
+   link: string
+   name: string
+   tag: string
+   role: string
+   opinion: string
+}
+
+const Users = ({ link, name, tag, role, opinion }: UsersProps) => {
    return (
-      <div>Users</div>
-   )
+   <>
+      <div id='user-profile' className='flex flex-row w-full'>
+         <div className='flex flex-col'>
+            <Link href={link!} className='space-x-2'>
+               <span className='font-poppins text-lg'>
+                  {name}
+               </span>
+               <span className='font-poppins'>
+                  @{tag}
+               </span>
+            </Link>
+            <div className='font-outfit'>
+               <span>{role}</span>
+            </div>
+         </div>
+      </div>
+      <div>
+         {opinion}
+      </div>
+   </>
+)
 }
 
 export default Users
