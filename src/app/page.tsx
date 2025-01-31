d931aa19f0446daad4ac3434e0e508dfe6db42a2
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-31 22:30:47 +0700

    feat<>: add metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
+import { Metadata } from "next";
 import { METADATA } from "@/common/constants/metadata";
+import { WithContext, Person } from "schema-dts";
 import Container from "@/components/elements/Container";
 import StructuredData from "@/components/elements/StructuredData";
 import Home from "@/modules/home/index";
-import { Metadata } from "next";
-
-import { WithContext, Person } from "schema-dts";
 
 export const metadata: Metadata = {
 	title: `${METADATA.creator} | Personal Website`,
 	alternates: {
-		canonical: ''
-	}
+		canonical: process.env.DOMAIN
+	},
+	keywords: METADATA.keyword
 }
 
 function generateStructureData(): WithContext<Person> {
