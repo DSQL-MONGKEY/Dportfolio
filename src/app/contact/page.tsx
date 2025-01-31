52c0c17e6e4f78bc260d0e1e0057d41992d7ff2a
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-31 22:30:55 +0700

    feat<>: add metadata

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,17 @@
+import React from 'react'
 import Container from '@/components/elements/Container'
 import Contact from '@/modules/contact'
-import React from 'react'
+import { Metadata } from "next";
+import { METADATA } from "@/common/constants/metadata";
+
+export const metadata: Metadata = {
+	title: `Contact ${METADATA.exTitle}`,
+	description: `Get in touch with me`,
+	alternates: {
+		canonical: `${process.env.DOMAIN}/contact`
+	},
+   keywords: 'software engineer, frontend developer, freelance web developer, instagram, tiktok, reactjs, nextjs, javasript, typescript, contact'
+}
 
 const ContactPage = () => {
    return (
