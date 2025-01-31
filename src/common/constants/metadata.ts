1d11cb1528f4b6af9af4a3a33b1ce6bb5b15459f
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-31 22:31:50 +0700

    feat<>: create metadata for metadata seo

---
+++ src/common/constants/metadata.ts
@@ -0,0 +1,16 @@
+export const METADATA = {
+   creator: 'Dimas Prasetyo',
+   description: 'Portfolio Website, Personal Website, portfolio, blog, software engineer journey, programming, macro-excel engine, belajar typescript, frontend engineer',
+   keyword: 'dimpfe, dimpfes, belajar typescript, frontend developer, frontend engineer',
+   authors: {
+      name: 'Dimas Prasetyo',
+      url: 'https://dimpfes.vercel.app/'
+   },
+   openGraph: {
+      url: '',
+      siteName: 'dimpfes',
+      locale: 'id-ID',
+   },
+   exTitle: '| Dimpfes',
+   profile: 'https://res.cloudinary.com/dbdcloud/image/upload/v1738291479/logo_afebmr.png'
+}
