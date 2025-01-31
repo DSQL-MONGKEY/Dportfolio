e8a68ff216b2b551a32f5119cbaac2573d02f08d
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-31 15:06:06 +0700

    feat<>: add metadat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,46 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import Layouts from "@/components/layouts";
 import ThemeProviderContext from "@/stores/theme";
 import { Suspense } from "react";
+import { METADATA } from "@/common/constants/metadata";
 
 
 export const metadata: Metadata = {
-	title: "Dimas Prasetyo | Portfolio Website",
-	description: "Created with love",
-	
+	applicationName: "dimpfes",
+	manifest: '/manifest.json',
+	appleWebApp: {
+		title: 'dimpfes',
+		capable: true,
+		statusBarStyle: 'default'
+	},
+	formatDetection: {
+		telephone: false,
+	},
+	// metadataBase: new URL(''),
+	description: METADATA.description,
+	keywords: METADATA.keyword,
+	creator: METADATA.creator,
+	authors: {
+		name: METADATA.authors.name,
+		url: METADATA.authors.url
+	},
+	openGraph: {
+		images: METADATA.profile,
+		url: METADATA.openGraph.url,
+		siteName: METADATA.openGraph.siteName,
+		locale: METADATA.openGraph.locale,
+		type: 'website'
+	},
+	robots: {
+		index: true,
+		follow: true,
+	}
 };
+
+export const themeColor:Viewport = {
+	themeColor: '#1D2B53'
+}
 
 export default function RootLayout({
   children,
@@ -18,7 +49,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" suppressHydrationWarning={true}>
-			<body className="bg-main dark:bg-mainDark transition-colors ease-in-out duration-300">
+			<body className="bg-main dark:bg-mainDark transition-colors ease-in-out duration-75">
 				<ThemeProviderContext>
 					<Suspense>
 						<Layouts>
