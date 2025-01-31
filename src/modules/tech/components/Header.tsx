0d008c262382d844aa477229adc769af32ab18e1
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-31 22:32:06 +0700

    chore<>: fix type

--- src/modules/tech/components/Header.tsx
+++ src/modules/tech/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
          <div className="flex flex-col gap-4 relative justify-center items-center p-5 bg-neutral-100 dark:bg-zinc-800 -translate-y-[8px] -translate-x-[8px] hover:translate-y-[0px] hover:translate-x-[0px] transition-all ease-in-out duration-200 rounded-lg border-2 border-black dark:border-neutral-100 select-none">
             <div>
                <h1 className="font-paytone text-4xl sm:text-5xl md:text-6xl text-neutral-700 dark:text-neutral-100">
-                  Techy and Tools
+                  Tech and Tools
                </h1>
             </div>
             <div>
