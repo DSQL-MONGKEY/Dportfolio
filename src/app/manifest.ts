601c15fa2a11c3e0975ba3ea02efcfb6356e81bd
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-31 22:30:39 +0700

    feat<>: change logo and add new keywords

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -4,16 +4,26 @@ export default function manifest():MetadataRoute.Manifest {
    return {
       name: 'Dimas Prasetyo - Software Engineer',
       short_name: 'Dimas Prasetyo',
-      description: 'Portfolio Website, Personal Website, portfolio, software engineer journey, neobrutalism design, dimpfe portfolio',
+      description: 'Portfolio Website, Personal Website, portfolio, software engineer journey, neobrutalism design, dimpfe portfolio, Dimas Prasetyo, Dimas Prasetyo UG',
       display: 'standalone',
       background_color: '#F6F54D',
       theme_color: '#F6F54D',
       icons: [
          {
-            src: '/logo/logo.png',
+            src: '/img/logo-192.png',
             sizes: '192x192',
             type: 'image/png'
-         }
+         },
+         {
+            src: '/img/logo-384.png',
+            sizes: '384x384',
+            type: 'image/png'
+         },
+         {
+            src: '/img/logo-512.png',
+            sizes: '192x192',
+            type: 'image/png'
+         },
       ]
    }
 }
