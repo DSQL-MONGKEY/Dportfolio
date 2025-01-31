0c4183627fccbb8b5feeb7ba776520f0b821d40d
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-31 22:31:10 +0700

    feat<>: add metadata

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,17 @@
+import React from 'react'
 import Container from '@/components/elements/Container'
 import Projects from '@/modules/projects'
-import React from 'react'
+import { Metadata } from "next";
+import { METADATA } from "@/common/constants/metadata";
+
+export const metadata: Metadata = {
+	title: `Projects ${METADATA.exTitle}`,
+	description: `The projects that i've working on`,
+	alternates: {
+		canonical: `${process.env.DOMAIN}/projects`
+	},
+   keywords: 'twitter clone, reactjs, nextjs, typescript, javascript, mongodb, mern stack, laravel, fullstack, notion clone, projects'
+}
 
 const ProjectsPage = () => {
    return (
