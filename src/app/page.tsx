761e7601693d90a800c3969ab349ad5a82f400b5
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-31 15:06:51 +0700

    feat<>: add metadata for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,34 @@
+import { METADATA } from "@/common/constants/metadata";
 import Container from "@/components/elements/Container";
+import StructuredData from "@/components/elements/StructuredData";
 import Home from "@/modules/home/index";
+import { Metadata } from "next";
+
+import { WithContext, Person } from "schema-dts";
+
+export const metadata: Metadata = {
+	title: `${METADATA.creator} | Personal Website`,
+	alternates: {
+		canonical: ''
+	}
+}
+
+function generateStructureData(): WithContext<Person> {
+	return {
+		'@context': 'https://schema.org',
+		'@type': 'Person',
+		name: METADATA.authors.name,
+		url: METADATA.authors.url,
+		image: METADATA.profile,
+		jobTitle: 'Software Engineer',
+		gender: 'Male'
+	}
+}
 
 export default function HomePage() {
 	return (
 		<>
+			<StructuredData data={generateStructureData()} />
 			<Container data-aos="fade-left">
 				<Home />
 			</Container>
