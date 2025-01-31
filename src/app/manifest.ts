faf6198bc66a32b11bed5f1d075d941da88c6c51
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-31 15:06:33 +0700

    feat<>: add metadata manifest atribut

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -3,6 +3,17 @@ import { MetadataRoute } from 'next'
 export default function manifest():MetadataRoute.Manifest {
    return {
       name: 'Dimas Prasetyo - Software Engineer',
-   
+      short_name: 'Dimas Prasetyo',
+      description: 'Portfolio Website, Personal Website, portfolio, software engineer journey, neobrutalism design, dimpfe portfolio',
+      display: 'standalone',
+      background_color: '#F6F54D',
+      theme_color: '#F6F54D',
+      icons: [
+         {
+            src: '/logo/logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+         }
+      ]
    }
 }
