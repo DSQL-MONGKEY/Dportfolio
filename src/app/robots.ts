a071e909894c5a23f8c60475b2db9b5f44f2c9c3
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-31 22:30:08 +0700

    feat<>: create robots file

---
+++ src/app/robots.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from "next";
+
+
+export default function robots(): MetadataRoute.Robots {
+   const domain = process.env.DOMAIN || 'http://www.dimpfes.com'
+   return {
+      rules: {
+         userAgent: '*',
+         allow: '/',
+         disallow: '/private/'
+      },
+      sitemap: `${domain}/sitemap.xml`
+   }
+}
