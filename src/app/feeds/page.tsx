d7becd4374ee0ea6581401a28bcb6191b12957cc
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-31 22:30:59 +0700

    feat<>: add metadata

--- src/app/feeds/page.tsx
+++ src/app/feeds/page.tsx
@@ -1,6 +1,17 @@
+import React from 'react'
 import Container from '@/components/elements/Container'
 import Feeds from '@/modules/feeds'
-import React from 'react'
+import { Metadata } from "next";
+import { METADATA } from "@/common/constants/metadata";
+
+export const metadata: Metadata = {
+	title: `Feeds ${METADATA.exTitle}`,
+	description: `Something might you like it`,
+	alternates: {
+		canonical: `${process.env.DOMAIN}/feeds`
+	},
+   keywords: 'chatting app, social feeds, chat, feeds'
+}
 
 const FeedsPage = () => {
    return (
