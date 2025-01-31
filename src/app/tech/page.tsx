3a52f9eeefe49bf3a29f9fde3352f945a0d74c35
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-31 22:31:14 +0700

    feat<>: add metadata

--- src/app/tech/page.tsx
+++ src/app/tech/page.tsx
@@ -1,12 +1,26 @@
+import React from 'react'
+import { Metadata } from "next";
+import { METADATA } from "@/common/constants/metadata";
 import Container from '@/components/elements/Container'
 import Tech from '@/modules/tech'
-import React from 'react'
+
+export const metadata: Metadata = {
+	title: `Tech ${METADATA.exTitle}`,
+	description: `Tech and tools that used to build your request`,
+	alternates: {
+		canonical: `${process.env.DOMAIN}/tech`
+	},
+   keywords: 'software engineer, frontend developer, reactjs, nextjs, javasript, typescript, tech and tools'
+}
+
 
 const TechPage = () => {
    return (
-      <Container data-aos="fade-left">
-         <Tech />
-      </Container>
+      <>
+         <Container data-aos="fade-left">
+            <Tech />
+         </Container>
+      </>
    )
 }
 
