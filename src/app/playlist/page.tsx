d9b82394099f5ee4994732183becc4173d581c54
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-31 22:31:06 +0700

    feat<>: add metadata

--- src/app/playlist/page.tsx
+++ src/app/playlist/page.tsx
@@ -1,6 +1,17 @@
+import React from 'react'
 import Container from '@/components/elements/Container'
 import Playlist from '@/modules/playlist'
-import React from 'react'
+import { Metadata } from "next";
+import { METADATA } from "@/common/constants/metadata";
+
+export const metadata: Metadata = {
+	title: `Playlist ${METADATA.exTitle}`,
+	description: `Hope you enjoy with my playlist`,
+	alternates: {
+		canonical: `${process.env.DOMAIN}/playlist`
+	},
+   keywords: 'music, playlist, music box, bruno mars, never enough'
+}
 
 const PlaylistPage = () => {
    return (
