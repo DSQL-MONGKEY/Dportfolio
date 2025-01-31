fa4b4ecde8f64aad145fcef94ce0fe908fc7a2ed
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-31 22:31:03 +0700

    feat<>: add metadata

--- src/app/journeys/page.tsx
+++ src/app/journeys/page.tsx
@@ -1,6 +1,17 @@
+import React from 'react'
 import Container from '@/components/elements/Container'
 import Journeys from '@/modules/journey'
-import React from 'react'
+import { Metadata } from "next";
+import { METADATA } from "@/common/constants/metadata";
+
+export const metadata: Metadata = {
+	title: `Journeys ${METADATA.exTitle}`,
+	description: `My journeys`,
+	alternates: {
+		canonical: `${process.env.DOMAIN}/journeys`
+	},
+   keywords: 'software engineering, bangkit academy, cimb niaga, software tester, lembaga pengembangan komputer univeristas gunadarma, lepkom ug, smk citra negara, journeys'
+}
 
 const JourneysPage = () => {
    return (
