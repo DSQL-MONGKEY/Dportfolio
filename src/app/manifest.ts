d483012b50d03623f5ea93ce47c964b7559162f7
Dimpfe <dimasprasetyo7696@gmail.com>
2025-02-01 10:44:16 +0700

    feat<>: remove icons metadata atribut

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -8,22 +8,5 @@ export default function manifest():MetadataRoute.Manifest {
       display: 'standalone',
       background_color: '#F6F54D',
       theme_color: '#F6F54D',
-      icons: [
-         {
-            src: '/img/logo-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-         },
-         {
-            src: '/img/logo-384.png',
-            sizes: '384x384',
-            type: 'image/png'
-         },
-         {
-            src: '/img/logo-512.png',
-            sizes: '192x192',
-            type: 'image/png'
-         },
-      ]
    }
 }
