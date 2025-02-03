5525dd1412ce90a4ff83c1bb73a4e4bee9db97cd
Dimpfe <dimasprasetyo7696@gmail.com>
2025-02-03 19:35:16 +0700

    chore<>: blank

--- src/modules/projects/components/Showcase.tsx
+++ src/modules/projects/components/Showcase.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Showcase = () => {
    return (
       <div className="flex flex-col">
-         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, omnis. Expedita debitis quia culpa nesciunt, porro id iste corrupti esse omnis illo blanditiis molestias iure architecto reiciendis nostrum sunt distinctio.
+         
       </div>
    )
 }
