c3edaed996d3186d77244c706982478b19c72b0f
Dimpfe <dimasprasetyo7696@gmail.com>
2025-02-05 16:23:59 +0700

    feat<>: change layout and create tabs feature for each categories

--- src/modules/tech/components/Board.tsx
+++ src/modules/tech/components/Board.tsx
@@ -1,22 +1,67 @@
-import { skillSet } from '@/common/constants/constants'
+"use client"
+
 import React from 'react'
-import Card from './Card'
+import ItemCard from './ItemCard'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/Tabs'
+import { devOps, tech, tools } from '@/common/constants/constants'
+import PulseNotif from '@/components/elements/PulseNotif'
 
 const Board = () => {
+   
 
 
    return (
-      <div className="grid grid-cols-4 md:flex md:flex-wrap md:justify-around p-2 border-2 border-black dark:border-neutral-200 rounded-lg no-scrollbar h-72 max-h-96 overflow-y-scroll overflow-x-hidden mt-5">
-            {skillSet.map((skill, index) => (
-               <Card 
-                  key={index} 
-                  title={skill.title} 
-                  id={index} 
-                  label={skill.label} 
-                  icon={skill.icon} 
-                  level={skill.level} 
-               />
-            ))}
+      <div className="flex flex-col justify-center w-full gap-6 my-5">
+         <Tabs defaultValue='tech' className='flex flex-col justify-center'>
+            <TabsList className='flex justify-around'>
+               <TabsTrigger value='tech'>Techs</TabsTrigger>
+               <TabsTrigger value='tools'>Tools</TabsTrigger>
+               <TabsTrigger value='devops'>DevOps</TabsTrigger>
+            </TabsList>
+            <TabsContent value='tech' className='grid grid-cols-6 sm:grid-cols-9 gap-5'>
+               {tech.map((item, idx) => (
+                  <ItemCard
+                     key={item.title}
+                     title={item.title}
+                     id={idx}
+                     label={item.label}
+                     icon={item.icon}
+                     level={item.level}
+                     onLearn={item.onLearn}
+                  />
+               ))}
+            </TabsContent>
+            <TabsContent value='tools' className='grid grid-cols-6 sm:grid-cols-9 gap-5'>
+               {tools.map((item, idx) => (
+                  <ItemCard
+                     key={item.title}
+                     title={item.title}
+                     id={idx}
+                     label={item.label}
+                     icon={item.icon}
+                     level={item.level}
+                     onLearn={item.onLearn}
+                  />
+               ))}
+            </TabsContent>
+            <TabsContent value='devops' className='grid grid-cols-6 sm:grid-cols-9 gap-5'>
+               {devOps.map((item, idx) => (
+                  <ItemCard
+                     key={item.title}
+                     title={item.title}
+                     id={idx}
+                     label={item.label}
+                     icon={item.icon}
+                     level={item.level}
+                     onLearn={item.onLearn}
+                  />
+               ))}
+            </TabsContent>
+         </Tabs>
+         <PulseNotif 
+            title='Currently Learning' 
+            className='bg-calmBlue dark:bg-green-500' 
+         />
       </div>
    )
 }
