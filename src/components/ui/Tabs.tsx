441f65fe575cff3d7f0e30dae9f722e224278dad
Dimpfe <dimasprasetyo7696@gmail.com>
2025-02-05 16:23:22 +0700

    chore<>: change style color

--- src/components/ui/Tabs.tsx
+++ src/components/ui/Tabs.tsx
@@ -15,7 +15,7 @@ React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
 <TabsPrimitive.List
    ref={ref}
    className={cn(
-      'inline-flex h-12 items-center justify-center rounded-base border-2 border-border bg-main p-1 text-mtext',
+      'inline-flex h-12 items-center justify-center rounded-md border-2 border-black bg-neutral-100 dark:bg-lightGreen p-1 text-center',
       className,
    )}
    {...props}
@@ -30,7 +30,7 @@ React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
 <TabsPrimitive.Trigger
    ref={ref}
    className={cn(
-      'inline-flex items-center justify-center whitespace-nowrap rounded-base border-2 border-transparent px-3 py-1.5 text-sm font-heading ring-offset-white transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:border-border data-[state=active]:bg-main',
+      'inline-flex items-center justify-center whitespace-nowrap rounded-md  px-3 py-1.5 text-sm font-heading transition-all w-full  focus-visible:ring-black focus-visible:outline-none  data-[state=active]:border-2 data-[state=active]:border-black data-[state=active]:border-b-4 data-[state=active]:border-r-6 data-[state=active]:bg-main dark:data-[state=active]:bg-zinc-800 dark:data-[state=active]:text-neutral-200 dark:text-black',
       className,
    )}
    {...props}
@@ -45,7 +45,7 @@ React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
 <TabsPrimitive.Content
    ref={ref}
    className={cn(
-      'mt-2 ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2',
+      'mt-2  focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2',
       className,
    )}
    {...props}
