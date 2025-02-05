febc788cacb888a1c65e2571e598ba58e9c9c3c5
Dimpfe <dimasprasetyo7696@gmail.com>
2025-02-05 08:18:52 +0700

    feat<>: change file name into itemCard

--- src/modules/tech/components/ItemCard.tsx
+++ src/modules/tech/components/ItemCard.tsx
@@ -7,19 +7,19 @@ interface CardProps {
    label: string,
    icon: JSX.Element,
    level: string,
+   onLearn: boolean
 }
 
 
 
-const Card = ({ title, id, label, icon, level }: CardProps) => {
+const ItemCard = ({ title, id, label, icon, level, onLearn }: CardProps) => {
    return (
       <div key={id+level} id={label} className="flex justify-center items-center flex-col gap-2 p-4 dark:border-neutral-200  rounded-lg">
          <span className="text-2xl">
             {icon}
          </span>
-         <p className="font-poppins text-md">{title}</p>
       </div>
    )
 }
 
-export default Card
+export default ItemCard
