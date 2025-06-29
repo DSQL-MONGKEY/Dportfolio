c29979a6650ce2469222a867131b9db7ec485855
Dimpfe <dimasprasetyo7696@gmail.com>
2025-06-29 12:08:30 +0700

    feat<>: create card showcase

--- src/modules/projects/components/Showcase.tsx
+++ src/modules/projects/components/Showcase.tsx
@@ -1,9 +1,18 @@
+import { projects } from '@/common/constants/constants'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/Card'
 import React from 'react'
 
 const Showcase = () => {
    return (
-      <div className="">
-         
+      <div className="grid grid-cols-3 gap-3">
+         {projects.map((item, idx) => (
+            <Card key={idx}>
+               <CardHeader>
+                  <CardTitle>{item.title}</CardTitle>
+               </CardHeader>
+               <CardContent>{item.desc}</CardContent>
+            </Card>
+         ))}
       </div>
    )
 }
