5c2211af1217493d6512a07d82d4915c96ba4779
Dimpfe <dimasprasetyo7696@gmail.com>
2025-08-02 00:30:10 +0700

    feat<>: add header component

--- src/modules/journey/components/Journeys.tsx
+++ src/modules/journey/components/Journeys.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Timeline from './Timeline';
+import Header from './Header';
 
 const Journeys = () => {
    return (
       <>
+         <Header />
          <Timeline />
-         
       </>
    )
 }
