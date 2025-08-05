3e1b5fec1b01fba70c425c0de16308cc20b07b44
Dimpfe <dimasprasetyo7696@gmail.com>
2025-08-05 15:04:44 +0700

    feat<>: add new music

--- src/common/constants/music.ts
+++ src/common/constants/music.ts
@@ -9,6 +9,36 @@ export const musicPlaylist = [
    },
    {
       musicCover: '',
+      src:   '/music/H.E.R. - Best Part Ft. Daniel Caesar.mp3',
+      artist: 'H.E.R. - Daniel Caesar',
+      title: 'Best Part - H.E.R Ft. Daniel Caesar'
+   },
+   {
+      musicCover: '',
+      src:   '/music/Love In The Dark - Adele.mp3',
+      artist: 'Adele',
+      title: 'Love In The Dark - Adele'
+   },
+   {
+      musicCover: '',
+      src:   '/music/if u could see me cryin in my room - Arash Buana, Raissa Anggiani.mp3',
+      artist: 'Arash Buana - Raissa Anggiani',
+      title: 'if u could see me cryin in my room - Arash Buana, Raissa Anggiani'
+   },
+   {
+      musicCover: '',
+      src:   '/music/BATAS SENJA - NANTI KITA SEPERTI INI - Batas Senja.mp3',
+      artist: 'BATAS SENJA',
+      title: 'NANTI KITA SEPERTI INI - BATAS SENJA'
+   },
+   {
+      musicCover: '',
+      src:   '/music/Opick - Ya Maulana.mp3',
+      artist: 'BATAS SENJA',
+      title: 'NANTI KITA SEPERTI INI - BATAS SENJA'
+   },
+   {
+      musicCover: '',
       src:   '/music/Runtuh.mp3',
       artist: 'Feby Putri ft Fiersa Besari',
       title: 'Runtuh - Feby Putri ft Fiersa Besari',
