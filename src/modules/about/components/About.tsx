bc3e37f7db3bd57338c398531c2ba1d2a558a773
Dimpfe <dimasprasetyo7696@gmail.com>
2025-08-06 14:27:05 +0700

    feat<>: add header component

--- src/modules/about/components/About.tsx
+++ src/modules/about/components/About.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Description from './Description'
 import Breakline from '@/components/elements/Breakline'
+import Header from './Header'
 
 const About = () => {
    return (
       <>
+         <Header />
          <Description />
          <Breakline />
       </>
