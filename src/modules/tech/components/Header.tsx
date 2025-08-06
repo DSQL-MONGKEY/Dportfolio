a46a2d93f39f12bde5b5bbffadcea0162aa069ea
Dimpfe <dimasprasetyo7696@gmail.com>
2025-08-06 14:28:39 +0700

    style<>: change text color

--- src/modules/tech/components/Header.tsx
+++ src/modules/tech/components/Header.tsx
@@ -1,18 +1,16 @@
-import SectionHeading from '@/components/elements/SectionHeading'
-import SectionSubHeading from '@/components/elements/SectionSubHeading'
-import React from 'react'
-import { GiTechnoHeart } from "react-icons/gi";
+import { GiTechnoHeart } from "react-icons/gi"
 
 const Header = () => {
    return (
-      <div>
-         <SectionHeading 
-            title='Techys' 
-            icon={<GiTechnoHeart />} 
-         />
-         <SectionSubHeading>
+      <div className="mb-5">
+         <div className="flex gap-1 space-y-2">
+            <GiTechnoHeart className="h-10 w-10"/>
+            <h2 className="text-3xl font-lexend text-neutral-900 dark:text-slate-200">Techys</h2>
+         </div>
+         <p className="font-outfit text-md text-neutral-700 dark:text-slate-300">
             Here&apos;s a trail of technologies and devices that I&apos;ve conquered - every single one of them is a silent witness of learning, challenges and achievements. This is not just a list, but a story of how I grew and adapted.
-         </SectionSubHeading>
+         </p>
+         
       </div>
    )
 }
