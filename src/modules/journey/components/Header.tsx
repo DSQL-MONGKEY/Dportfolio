a2f7c7e5660eb6a68759e094bcbd3a7a6fa0e8f2
Dimpfe <dimasprasetyo7696@gmail.com>
2025-08-06 14:27:56 +0700

    style<>: change color

--- src/modules/journey/components/Header.tsx
+++ src/modules/journey/components/Header.tsx
@@ -9,8 +9,8 @@ const Header = () => {
             <FaWalking className="h-10 w-10"/>
             <h2 className="text-3xl font-lexend text-neutral-900 dark:text-slate-200">Journey</h2>
          </div>
-         <p className="font-outfit text-md text-neutral-900 dark:text-slate-200 ">
-            <span className='bg-slate-400 dark:bg-cyan-500'>
+         <p className="font-outfit text-md text-neutral-700 dark:text-slate-300">
+            <span className='bg-cyan-300 dark:bg-cyan-800'>
                The Journey that Shaped Who I Am Today
             </span>
             , each step below is not just a career change - it&apos;s a trail of courage, self-discovery, and a willingness to grow from ground zero. This is not just a timeline, but a reflection on the courage to metamorphose.
