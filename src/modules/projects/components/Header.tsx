317418e5ad72a7ab5ad109e49b5d036ea1fd87df
Dimpfe <dimasprasetyo7696@gmail.com>
2025-08-06 14:28:42 +0700

    style<>: change text color

--- src/modules/projects/components/Header.tsx
+++ src/modules/projects/components/Header.tsx
@@ -1,18 +1,16 @@
+import { LucideSquareSigma } from 'lucide-react'
 import React from 'react'
-import SectionHeading from '@/components/elements/SectionHeading'
-import { GoProjectTemplate } from "react-icons/go";
-import SectionSubHeading from '@/components/elements/SectionSubHeading';
 
 const Header = () => {
    return (
-      <div className='flex flex-col'>
-         <SectionHeading
-            title='Projects'
-            icon={<GoProjectTemplate />}
-         />
-         <SectionSubHeading>
-            <p>Few projects that i&apos;ve worked on so far</p>
-         </SectionSubHeading>
+      <div className="mb-5">
+         <div className="flex gap-1 space-y-2">
+            <LucideSquareSigma className="h-10 w-10"/>
+            <h2 className="text-3xl font-lexend text-neutral-900 dark:text-slate-200">Projects</h2>
+         </div>
+         <p className="font-outfit text-md text-neutral-700 dark:text-slate-300">
+            Rather than “projects”, these are stories that I built. Every single one of them is the result of crazy ideas, sweat, and endless innovation. This is not just a list, but a tangible proof of my journey and all that I&apos;ve accomplished.
+         </p>
       </div>
    )
 }
