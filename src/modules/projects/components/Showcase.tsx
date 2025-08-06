a6ff4c49a832a5a9c8a6104707fb83dbef5d25c8
Dimpfe <dimasprasetyo7696@gmail.com>
2025-08-06 14:28:28 +0700

    feat<>: add itemProject component

--- src/modules/projects/components/Showcase.tsx
+++ src/modules/projects/components/Showcase.tsx
@@ -1,17 +1,19 @@
 import { projects } from '@/common/constants/constants'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/Card'
 import React from 'react'
+import ItemProject from './ItemProject'
 
 const Showcase = () => {
    return (
-      <div className="grid grid-cols-3 gap-3">
+      <div className="flex flex-col gap-5">
          {projects.map((item, idx) => (
-            <Card key={idx}>
-               <CardHeader>
-                  <CardTitle>{item.title}</CardTitle>
-               </CardHeader>
-               <CardContent>{item.desc}</CardContent>
-            </Card>
+            <ItemProject
+               key={idx}
+               title={item.title}
+               techStack={item.techStack}
+               desc={item.desc}
+               link={item.link}
+               isFeatured={item.isFeatured}
+            />
          ))}
       </div>
    )
