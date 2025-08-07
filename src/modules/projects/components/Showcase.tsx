66b3fbac24940e6653401b386877efebaf2b075e
Dimpfe <dimasprasetyo7696@gmail.com>
2025-08-07 21:35:41 +0700

    feat<>: change project map and add sorting by featured

--- src/modules/projects/components/Showcase.tsx
+++ src/modules/projects/components/Showcase.tsx
@@ -5,7 +5,10 @@ import ItemProject from './ItemProject'
 const Showcase = () => {
    return (
       <div className="flex flex-col gap-5">
-         {projects.map((item, idx) => (
+         {projects.sort((x, y) => (
+            x.isFeatured === y.isFeatured
+         ) ? 0 : x.isFeatured ? -1 : 0)
+         .map((item, idx) => (
             <ItemProject
                key={idx}
                title={item.title}
